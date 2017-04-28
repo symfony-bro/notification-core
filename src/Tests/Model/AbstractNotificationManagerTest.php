c6630c02ce103b06412666043ebc64a7e8fa1d79
Pavel Stepanets <pahhan.ne@gmail.com>
2017-04-28 18:25:45 +0700

    add exception tests

--- src/Tests/Model/AbstractNotificationManagerTest.php
+++ src/Tests/Model/AbstractNotificationManagerTest.php
@@ -7,6 +7,7 @@
 namespace SymfonyBro\NotificationCore\Tests\Model;
 
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 use SymfonyBro\NotificationCore\Model\AbstractNotificationManager;
 use SymfonyBro\NotificationCore\Model\DriverInterface;
@@ -60,24 +61,91 @@ class AbstractNotificationManagerTest extends TestCase
         $manager->notify($notification);
     }
 
+    public function testNotifyWithExceptions()
+    {
+        $notification = $this->getMockForAbstractClass(NotificationInterface::class);
+
+        /** @var AbstractNotificationManager|\PHPUnit_Framework_MockObject_MockObject $manager */
+        $manager = $this->getMockBuilder(AbstractNotificationManager::class)
+            ->setMethods([
+                'beforeFormat',
+                'createFormatters',
+                'createDriver',
+                'beforeSend',
+                'onFormatException',
+                'onDriverCreateException',
+                'onSendException',
+            ])
+            ->getMockForAbstractClass()
+        ;
+
+        $message1 = $this->getMockForAbstractClass(MessageInterface::class);
+        $formatException =  new Exception('It\'s too bad');
+        $message2 = $this->getMockForAbstractClass(MessageInterface::class);
+
+        $formatters = [
+            $this->createFormatter($notification, $message1),
+            $this->createFormatter($notification, $formatException),
+            $this->createFormatter($notification, $message2),
+        ];
+
+        $manager->expects($this->once())
+            ->method('createFormatters')
+            ->with($notification)
+            ->willReturn($formatters)
+        ;
+
+        $driverSendException = new Exception('Can\'t send message');
+        $driver1 = $this->createDriver($message1, $driverSendException);
+        $driverCreateException = new Exception('Something is broken in driver creation');
+        $manager->expects($this->exactly(count($formatters) - 1))
+            ->method('createDriver')
+            ->withConsecutive($message1, $message2)
+            ->willReturnOnConsecutiveCalls($driver1, $this->throwException($driverCreateException))
+        ;
+
+        $manager->expects($this->once())
+            ->method('onFormatException')
+            ->with($notification, $formatException);
+
+        $manager->expects($this->once())
+            ->method('onDriverCreateException')
+            ->with($notification, $message2, $driverCreateException);
+
+        $manager->expects($this->once())
+            ->method('onSendException')
+            ->with($notification, $message1, $driverSendException);
+
+        $manager->notify($notification);
+    }
+
     private function createFormatter($notification, $message)
     {
         $formatter = $this->getMockForAbstractClass(FormatterInterface::class);
-        $formatter->expects($this->once())
+        $with = $formatter->expects($this->once())
             ->method('format')
             ->with($notification)
-            ->willReturn($message)
         ;
+
+        if ($message instanceof Exception) {
+            $with->willThrowException($message);
+        } else {
+            $with->willReturn($message);
+        }
+
         return $formatter;
     }
 
-    private function createDriver($message)
+    private function createDriver($message, $exception = null)
     {
         $driver = $this->getMockForAbstractClass(DriverInterface::class);
-        $driver->expects($this->once())
+        $with = $driver->expects($this->once())
             ->method('send')
             ->with($message)
         ;
+        if (null !== $exception) {
+            $with->willThrowException($exception);
+        }
         return $driver;
     }
 
